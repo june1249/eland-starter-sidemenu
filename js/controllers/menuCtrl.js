e7deedebade197ca51b2be7fe1ff76ec3757ee53
June <kliz78@gmail.com>
2016-11-16 21:07:23 +0900

    Change starter sidemenu templates

--- js/controllers/menuCtrl.js
+++ js/controllers/menuCtrl.js
@@ -1,3 +1,29 @@
-controllerModule.controller('MenuCtrl', function($scope) {
+controllerModule.controller('MenuCtrl', function($scope, $window, $ionicModal, $ionicSideMenuDelegate, MenuService) {
+
+  $scope.menus = [];
+  $scope.data = MenuService.all();
+  $scope.userInfo = $scope.data[0].data.accountInfo;
+  $scope.menus = $scope.data[0].data.sections;
+
+  $scope.showSettings = function() {
+      $scope.settings();
+      $ionicSideMenuDelegate.toggleLeft();
+  }
+
+  $ionicModal.fromTemplateUrl('templates/settings.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the settings modal to close it
+  $scope.closeSettings = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the settings modal
+  $scope.settings = function() {
+    $scope.modal.show();
+  };
 
 });
